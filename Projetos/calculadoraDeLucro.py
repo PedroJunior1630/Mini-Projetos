5333f74d492cb0456092fb0732619bfe5150f5bf
pedrojunior <ph751153@gmail.com>
2024-01-17 17:14:31 -0300

    Upgrade

--- Projetos/calculadoraDeLucro.py
+++ Projetos/calculadoraDeLucro.py
@@ -2,6 +2,8 @@ produtos = []
 produto = {}
 total = 0
 
+def erro():
+    print("\033[31mERRO! DIGITE UM VALOR NUMÉRICO\033[m")
 def tempo(d=0.8):
     from time import sleep
     sleep(d)
@@ -12,7 +14,7 @@ def leiaInt(msg):
         try:
             a = int(input(msg))
         except:
-            print("\033[31mERRO! DIGITE UM VALOR NUMÉRICO\033[m")
+            erro()
         else:
             return a
 
@@ -36,7 +38,7 @@ def moeda(coin):
         else:
             t = a.replace(',','.')
             if t.isalnum():
-                print("\033[31mERRO! DIGITE UM VALOR VÁLIDO!\033[m")
+                erro()
             else:
                 return float(t)
 
@@ -66,8 +68,10 @@ for valor in produtos:
     tempo(0.5)
 print(f'{"Total:":.<30} R${total:>5.2f}')
 custopp = total/fabrica
-cabecalho("CUSTO POR PRODUTO")
-print(f"R${custopp:.2f}")
+cabecalho("INFORMAÇÕES")
+print(f"R${total:.2f} fabricam {fabrica} {produtonome}(s).")
+print(f"R${total:.2f} / {fabrica} = R${custopp:.2f}")
+print(f"Custo por produto: R${custopp:.2f}")
 for c in range(10,151,10):
     print(f'{c} % de lucro, você vende por R${custopp + (custopp * c / 100):.2f} cada {produtonome}.')
     tempo(0.2)
