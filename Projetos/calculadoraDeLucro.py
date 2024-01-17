365e33275eb97d2d96bacc6cc18b59523c7934e7
pedrojunior <ph751153@gmail.com>
2024-01-17 13:22:29 -0300

    Parte interativa adicionado

---
+++ Projetos/calculadoraDeLucro.py
@@ -0,0 +1,54 @@
+produtos = []
+produto = {}
+
+def tempo():
+    from time import sleep
+    sleep(1.5)
+
+def leiaInt(msg):
+    while True:
+        try:
+            a = int(input(msg))
+        except:
+            print("\033[31mERRO! DIGITE UM VALOR NUMÉRICO\033[m")
+        else:
+            return a
+
+
+def linha(tam=20):
+    print("=+=" * tam)
+
+def cabecalho(msg):
+    linha()
+    print(msg.center(60))
+    linha()
+    tempo()
+
+def moeda(coin):
+    while True:
+        a = input(coin)
+        if a.isnumeric():
+            return float(a)
+        else:
+            t = a.replace(',','.')
+            if t.isalnum():
+                print("\033[31mERRO! DIGITE UM VALOR VÁLIDO!\033[m")
+            else:
+                return float(t)
+
+cabecalho("PRODUTO")
+produtonome = str(input("Qual nome do produto? "))
+qntd = leiaInt('Quantos ingredientes são necessários? ')
+linha()
+tempo()
+
+for c in range(0,qntd):
+    cabecalho(f"{c+1}° PRODUTO")
+    produto["nome"] = str(input(f'Nome do {c+1}° ingrediente: '))
+    produto["preço"] = moeda(f'Preço do {produto["nome"]} R$')
+    produtos.append(produto.copy())
+    produto.clear()
+linha()
+tempo()
+
+print(produtos)
