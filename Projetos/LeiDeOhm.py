a6ebd0b18b4dc199cfe239667d3ccec501009f82
pedrojunior <ph751153@gmail.com>
2024-02-03 17:58:01 -0300

    Tensao adicionada

--- Projetos/LeiDeOhm.py
+++ Projetos/LeiDeOhm.py
@@ -34,10 +34,51 @@ def layout(*item):
     opc = leiaInt('Qual opção Deseja?')
     return opc
 
+def pergunta(tipo):
+    v = int(input(f"Quantos {tipo}? "))
+    return v
+
+
 def conversao(tipo):
-    
+    if tipo == 1:
+        cabecalho("TENSÃO ELÉTRICA(VOLTS)")
+        print("QUAIS VALORES VOCÊ POSSUI?")
+        valores = [input("Amperagem?[S/N]").upper(),input("Resistência?[S/N]").upper(),input("Potência?[S/N]").upper()]
+        linha()
+        if valores[0] == "S" and valores[2] == "S":
+            wa = pergunta("Watts")
+            am = pergunta("Amperes")
+            linha()
+            print("V = P / A")
+            tempo(0.5)
+            print(f"V = {wa} / {am}")
+            tempo(0.5)
+            print(f"V = {wa/am:.2f}")
+            linha()
 
+        elif valores[0] == "S" and valores[1] == "S":
+            am = pergunta("Amperes")
+            re = pergunta("Ohms")
+            linha()
+            print("V = R * A")
+            tempo(0.5)
+            print(f"V = {am} x {re}")
+            tempo(0.5)
+            print(f"V = {am*re:.2f}")
+            linha()
 
+        elif valores[1] == "S"  and valores[2] == "S":
+            re = pergunta("Ohms")
+            po = pergunta("Watts")
+            linha()
+            print("V = P / A")
+            tempo(0.5)
+            print(f"V = √{po} x {re}")
+            tempo(0.5)
+            print(f"V = √{po*re}")
+            tempo(0.5)
+            print(f"V = {(po*re)**(1/2):.2f}")
+            linha()
 cabecalho("CONVERSÃO DE GRANDEZAS ELÉTRICAS")
 tempo(1)
 interface = layout("Tensão[Volts]", "Corrente[Amperes]","Resistência[Ohms]","Potência[Watts]")
