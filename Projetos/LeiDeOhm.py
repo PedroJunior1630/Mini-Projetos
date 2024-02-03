f37e2e1ed51235fa81dcf6b7134ed4f18861e1de
pedrojunior <ph751153@gmail.com>
2024-02-03 17:05:40 -0300

    Fução adicionada

--- Projetos/LeiDeOhm.py
+++ Projetos/LeiDeOhm.py
@@ -1,1 +1,22 @@
-def linha()
+def tempo(seg):
+    from time import sleep
+    sleep(seg)
+def linha(tam=20):
+    print("=+=" * tam)
+
+def cabecalho(msg):
+    linha()
+    print(msg.center(60))
+    linha()
+
+def layout(*item):
+    cabecalho("QUE TIPO DE GRANDEZA DESEJA DESCOBRIR?")
+    
+def conversao(tipo):
+    pass
+
+
+cabecalho("CONVERSÃO DE GRANDEZAS ELÉTRICAS")
+tempo(1)
+layout("Tensão(Volts)", "Corrente(Amperes)","Resistência(Ohms)","Potência(Watts)")
+
