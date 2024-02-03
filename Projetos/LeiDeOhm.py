7e14a84471fc1850e95c9494206c6ed1d9eb8f56
pedrojunior <ph751153@gmail.com>
2024-02-03 18:16:27 -0300

    Cores no terminal

--- Projetos/LeiDeOhm.py
+++ Projetos/LeiDeOhm.py
@@ -1,16 +1,21 @@
+AZUL = "\033[1;36m"
+
 def tempo(seg):
     from time import sleep
     sleep(seg)
 
 
 def linha(tam=20):
+    print(AZUL)
     print("=+=" * tam)
-
+    print("\033[m")
 
 def cabecalho(msg):
+    print("\033[1;33m")
     linha()
-    print(msg.center(60))
+    print(f"\033[1;33m {msg.center(60)}\033[m")
     linha()
+    print("\033[m")
 
 
 def leiaInt(inp):
@@ -28,14 +33,16 @@ def leiaInt(inp):
 
 def layout(*item):
     cabecalho("QUE TIPO DE GRANDEZA DESEJA DESCOBRIR?")
+    print("\033[1;16m")
     for num, grandeza in enumerate(item):
         print(f"[ {num+1} ] - {grandeza:<15}")
     print(f"[ 0 ] - Para finalizar.")
-    opc = leiaInt('Qual opção Deseja?')
+    print("\033[m")
+    opc = leiaInt('\033[1;97mQual opção Deseja?\033[m')
     return opc
 
 def pergunta(tipo):
-    v = int(input(f"Quantos {tipo}? "))
+    v = int(input(f"\033[1;33mQuantos {tipo}? \033[m"))
     return v
 
 
@@ -43,7 +50,10 @@ def conversao(tipo):
     if tipo == 1:
         cabecalho("TENSÃO ELÉTRICA(VOLTS)")
         print("QUAIS VALORES VOCÊ POSSUI?")
+        print("\033[1;34m")
         valores = [input("Amperagem?[S/N]").upper(),input("Resistência?[S/N]").upper(),input("Potência?[S/N]").upper()]
+        print("\033[m")
+        
         linha()
         if valores[0] == "S" and valores[2] == "S":
             wa = pergunta("Watts")
@@ -79,6 +89,8 @@ def conversao(tipo):
             tempo(0.5)
             print(f"V = {(po*re)**(1/2):.2f}")
             linha()
+
+
 cabecalho("CONVERSÃO DE GRANDEZAS ELÉTRICAS")
 tempo(1)
 interface = layout("Tensão[Volts]", "Corrente[Amperes]","Resistência[Ohms]","Potência[Watts]")
